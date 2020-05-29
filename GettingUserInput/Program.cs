45b0409206fe91545a529cd140fc05cf7dcb32ad
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-05-29 14:02:54 -0400

    Task: Creating a way to input for user

---
+++ GettingUserInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GettingUserInput
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+            args = "";
+            string name;
+            string age;
+
+            Console.WriteLine("Enter your name: "); //This prompts user with screen
+             name = Console.ReadLine(); //This prompts user for input from Enter your name. The string name sets the Console.ReadLine as a variable
+            Console.WriteLine("Enter your age: "); //You can keep requesting info this way to interact with user
+             age = Console.ReadLine(); // set the string variable to age and the input is stored into variable age
+            Console.WriteLine("Hello " + name + " You are " + age + "years old " ); //This responds by saying hello and whatever the user put for name
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
